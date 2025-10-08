adf813067bdab71c8c464c3f7435f8672725d03f
Hammers <gianluca.hammers@gmail.com>
2025-10-08 15:27:59 +0200

    Refactor form structure and functionality in index.html and script.js; update styles for task list display

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const useBtn = () => {
-    let targetBtn = document.getElementById("btn"); /// selezionato <button></button>
-    
-};
+const form = document.getElementById("main-form");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const userThings = document.getElementById("things");
+  const createDiv = document.createElement("div"); // <div> </div>
+  const userValue = userThings.value;
+  createDiv.classList.add("div-matti");
+  const autoDistr = document.createElement("button");
+  autoDistr.innerHTML = "Delete";
+  createDiv.innerHTML = `
+  <p>${userValue} </p>
+    `;
+  form.appendChild(createDiv);
+
+  createDiv.addEventListener("click", (e) => {
+    e.target.style.textDecoration = "line-through";
+    e.target.style.color = "red";
+  });
+});
