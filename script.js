2a6fbc616e1cff4ea0e2e22c61f6285457d125b1
Hammers <gianluca.hammers@gmail.com>
2025-10-09 07:47:52 +0200

    Refactor delete button functionality in script.js; streamline event handling for task removal

--- script.js
+++ script.js
@@ -6,15 +6,21 @@ form.addEventListener("submit", function (e) {
   const createDiv = document.createElement("div"); // <div> </div>
   const userValue = userThings.value;
   createDiv.classList.add("div-matti");
-  const autoDistr = document.createElement("button");
-  autoDistr.innerHTML = "Delete";
   createDiv.innerHTML = `
   <p>${userValue} </p>
+  <button type="onclick" class="delete-btn">Delete</button>
     `;
   form.appendChild(createDiv);
 
   createDiv.addEventListener("click", (e) => {
     e.target.style.textDecoration = "line-through";
     e.target.style.color = "red";
+    e.classList.toggle("div-matti");
   });
+  const deleteBtn = createDiv.querySelector(".delete-btn");
+
+  deleteBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    createDiv.remove();
+  }); 
 });
